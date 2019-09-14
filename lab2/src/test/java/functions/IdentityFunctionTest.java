12432950d12aa420cd5bd27310eb98d724ced90c
Megrelishvili <bagrat37@mail.ru>
2019-09-14 18:34:40 +0400

    creating testing class for IdentityFunction

---
+++ lab2/src/test/java/functions/IdentityFunctionTest.java
@@ -0,0 +1,12 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdentityFunctionTest {
+
+    @Test
+    void apply() {
+    }
+}
