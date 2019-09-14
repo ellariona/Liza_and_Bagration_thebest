13f4aa2db1a927959a14092d63700fd40fd7e5f0
Megrelishvili <bagrat37@mail.ru>
2019-09-14 21:27:01 +0400

    updating test

---
+++ lab2/src/test/java/functions/CompositeFunctionTest.java
@@ -0,0 +1,12 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompositeFunctionTest {
+
+    @Test
+    void apply() {
+    }
+}
