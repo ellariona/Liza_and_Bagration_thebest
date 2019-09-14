dcc85b98a2adffeb6bb15b1a7c3c4c52449213d8
Megrelishvili <bagrat37@mail.ru>
2019-09-14 21:56:33 +0400

    updating test for CompositeFunctionTest

--- lab2/src/test/java/functions/CompositeFunctionTest.java
+++ lab2/src/test/java/functions/CompositeFunctionTest.java
@@ -6,7 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CompositeFunctionTest {
 
+    IdentityFunction identityFunction = new IdentityFunction();
+    SqrFunction sqrFunction = new SqrFunction();
+    CompositeFunction compositeFunction1 = new CompositeFunction(sqrFunction, identityFunction);
+    CompositeFunction compositeFunction2 = new CompositeFunction(compositeFunction1, identityFunction);
     @Test
-    void apply() {
+    void applyTest() {
+        double z1 = compositeFunction1.apply(3);
+        assertEquals(9, z1);
+        z1=compositeFunction1.apply(3);
+        assertEquals(9, z1);
     }
 }
