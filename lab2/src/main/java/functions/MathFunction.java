18a8a7cf492736fc4fe4f32807c6ace130556472
Елизавета <luhra@mail.ru>
2019-09-15 17:16:27 +0400

    вроде работает

--- lab2/src/main/java/functions/MathFunction.java
+++ lab2/src/main/java/functions/MathFunction.java
@@ -2,4 +2,6 @@ package functions;
 
 public interface MathFunction {
     double apply(double x);
+    default CompositeFunction andThen(MathFunction afterFunction) {
+        return new CompositeFunction(this, afterFunction);
 }
