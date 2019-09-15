2576f119e6578b1942117b2d815f47569c6ee716
Megrelishvili <bagrat37@mail.ru>
2019-09-15 15:23:32 +0400

    adding ExpFunction test

---
+++ lab2/src/test/java/functions/ExpFunctionTest.java
@@ -0,0 +1,14 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpFunctionTest {
+
+    @Test
+    void applyExp() {
+        ExpFunction expFunction = new ExpFunction();
+        assertEquals(Math.exp(3),expFunction.apply(3));
+    }
+}
