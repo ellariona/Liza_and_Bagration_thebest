8ebe15ac42c5c5fbcb36a5bf5941935161e72c92
Елизавета <luhra@mail.ru>
2019-09-28 00:58:25 +0400

    new new test and new new new test

---
+++ lab2/src/test/java/functions/ZeroFunctionTest.java
@@ -0,0 +1,20 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ZeroFunctionTest
+{
+    ZeroFunction testFunction = new ZeroFunction();
+    @Test
+    public void testApplyConstant()
+    {
+        assertEquals(testFunction.apply(4), 0, 0.0001);
+    }
+    @Test
+    public void testGetConstNumber()
+    {
+        assertEquals(testFunction.GetConstNumber(), 0, 0.0001);
+    }
+}
