034647df5b1001fc0ec552f2f7b907f0c4159008
Елизавета <luhra@mail.ru>
2019-09-28 11:27:45 +0400

    pactch v1.2

--- lab2/src/main/java/functions/LinkedListTabulatedFunction.java
+++ lab2/src/main/java/functions/LinkedListTabulatedFunction.java
@@ -2,7 +2,6 @@ package functions;
 
 public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
     private Node head;
-    private Node last;
     protected int count;
 
     private void addNode(double x, double y) {
@@ -37,12 +36,14 @@ public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
         }
         double stepX = (xTo - xFrom) / (count - 1);
         if (xFrom != xTo) {
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < count; i++) {
                 addNode(xFrom + stepX * i, source.apply(xFrom + stepX * i));
-        } else
+            }
+        } else {
             for (int i = 0; i < count; i++) {
                 addNode(xFrom, source.apply(xFrom));
             }
+        }
     }
 
     public int getCount() {
@@ -59,8 +60,9 @@ public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
 
     private Node getNode(int index) {
         Node finding = head;
-        for (int i = 0; i < index; i++)
+        for (int i = 0; i < index; i++) {
             finding = finding.next;
+        }
         return finding;
     }
 
@@ -78,52 +80,58 @@ public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
 
     public int indexOfX(double x) {
         for (int i = 0; i < count; i++) {
-            if (x == this.getX(i))
+            if (x == this.getX(i)) {
                 return i;
+            }
         }
         return -1;
     }
 
     public int indexOfY(double y) {
         for (int i = 0; i < count; i++) {
-            if (y == this.getY(i))
+            if (y == this.getY(i)) {
                 return i;
+            }
         }
         return -1;
     }
 
     @Override
     public int floorIndexOfX(double x) {
-        if (x < head.x)
+        if (x < head.x) {
             return 0;
-        if (x > head.prev.x)
+        }
+        if (x > head.prev.x) {
             return count;
+        }
         for (int i = 1; i < count; i++) {
-            if (x < getX(i))
+            if (x < getX(i)) {
                 return i - 1;
+            }
         }
         return count;
     }
 
     @Override
     protected double interpolate(double x, int floorIndex) {
-        if (count == 1)
+        if (count == 1) {
             return head.y;
+        }
         Node left = getNode(floorIndex);
         Node right = left.next;
-        return left.y + (right.y - left.y) / (right.x - left.x) * (x - left.x);
+        return super.interpolate(x, left.x, right.x, left.y, right.y);
     }
 
     @Override
     protected double extrapolateRight(double x) {
         Node left = head.prev.prev;
-        return super.interpolate(x,left.x,last.x,left.y,head.prev.y);
+        return super.interpolate(x, left.x, head.prev.x, left.y, head.prev.y);
     }
 
     @Override
     protected double extrapolateLeft(double x) {
         Node right = head.next;
-        return super.interpolate(x,head.x,right.x,head.y,right.y);
+        return super.interpolate(x, head.x, right.x, head.y, right.y);
     }
 }
 
