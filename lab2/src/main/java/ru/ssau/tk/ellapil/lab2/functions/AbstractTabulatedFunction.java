d51525b10ae3435e29858508aecfe1c57fa37e9d
Елизавета <luhra@mail.ru>
2019-10-11 22:46:59 +0400

    короче абстракт обновила, теперь он поновее выглядит (Багратион, зачем ты удалил мои веселые комментарии к коммитам:( Это был единственный лучик света в этом темном царстве :( )

--- lab2/src/main/java/ru/ssau/tk/ellapil/lab2/functions/AbstractTabulatedFunction.java
+++ lab2/src/main/java/ru/ssau/tk/ellapil/lab2/functions/AbstractTabulatedFunction.java
@@ -1,5 +1,8 @@
 package ru.ssau.tk.ellapil.lab2.functions;
 
+import ru.ssau.tk.ellapil.lab2.exceptions.ArrayIsNotSortedException;
+import ru.ssau.tk.ellapil.lab2.exceptions.DifferentLengthOfArraysException;
+
 public abstract class AbstractTabulatedFunction implements TabulatedFunction {
     abstract protected int floorIndexOfX(double x);
 
@@ -27,4 +30,18 @@ public abstract class AbstractTabulatedFunction implements TabulatedFunction {
 
     }
 
+    static void checkLengthIsTheSame(double[] xValues, double[] yValues) {
+        if (xValues.length != yValues.length) {
+            throw new DifferentLengthOfArraysException();
+        }
+    }
+
+    static void checkSorted(double[] xValues) {
+        for (int i = 1; i < xValues.length; i++) {
+            if (xValues[i] <= xValues[i - 1]) ;
+            {
+                throw new ArrayIsNotSortedException();
+            }
+        }
+    }
 }
