c87623164ae23ad11f4b253dad6ce9b02530d22d
Елизавета <luhra@mail.ru>
2019-10-25 19:54:48 +0400

    vfktymrjt bpvytybt ? xnj,s ,fuhfnbjy cvju dspdfnm b ytljhf,jnfyysq rkfcc
    ............................... клавиатура, за что ты так со мной... короче, кому надо, тот разгадает... мне лень переписать

--- lab2/src/main/java/ru/ssau/tk/ellapil/lab2/operations/TabulatedFunctionOperationService.java
+++ lab2/src/main/java/ru/ssau/tk/ellapil/lab2/operations/TabulatedFunctionOperationService.java
@@ -2,12 +2,23 @@ package ru.ssau.tk.ellapil.lab2.operations;
 
 import ru.ssau.tk.ellapil.lab2.functions.Point;
 import ru.ssau.tk.ellapil.lab2.functions.TabulatedFunction;
+import ru.ssau.tk.ellapil.lab2.functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
-    Point[] asPoints(TabulatedFunction tabulatedFunction) {
+    public TabulatedFunctionFactory factory;
+
+    TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    TabulatedFunctionOperationService() {
+
+    }
+
+    static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         int i = 0;
         Point[] points = new Point[tabulatedFunction.getCount()];
-        for(Point newPoint : tabulatedFunction) {
+        for (Point newPoint : tabulatedFunction) {
             points[i++] = newPoint;
         }
         return points;
