0ef43720505e45fd225e74eff4199137f6d898ad
Елизавета <luhra@mail.ru>
2019-10-29 11:17:15 +0400

    Merge branch 'master' of C:\Users\Елизавета\IdeaProjects\Liza_and_Bagration_thebest with conflicts.

--- lab2/src/main/java/ru/ssau/tk/ellapil/lab2/io/TabulatedFunctionFileWriter.java
+++ lab2/src/main/java/ru/ssau/tk/ellapil/lab2/io/TabulatedFunctionFileWriter.java
@@ -1,4 +1,11 @@
 package ru.ssau.tk.ellapil.lab2.io;
 
+import ru.ssau.tk.ellapil.lab2.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.ellapil.lab2.functions.ExpFunction;
+import ru.ssau.tk.ellapil.lab2.functions.LinkedListTabulatedFunction;
+
 public class TabulatedFunctionFileWriter {
+    public static void main(String[] args)
+    {
+    }
 }
