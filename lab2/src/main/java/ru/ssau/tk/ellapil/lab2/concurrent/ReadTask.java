9be4e05c2c4f1a41d7dacf8793b41bb64430ab87
Megrelishvili <bagrat37@mail.ru>
2019-11-22 00:48:51 +0400

    synchronized

--- lab2/src/main/java/ru/ssau/tk/ellapil/lab2/concurrent/ReadTask.java
+++ lab2/src/main/java/ru/ssau/tk/ellapil/lab2/concurrent/ReadTask.java
@@ -3,7 +3,7 @@ package ru.ssau.tk.ellapil.lab2.concurrent;
 import ru.ssau.tk.ellapil.lab2.functions.TabulatedFunction;
 
 public class ReadTask implements Runnable {
-    private TabulatedFunction tabulatedFunction;
+    private final TabulatedFunction tabulatedFunction;
 
     ReadTask(TabulatedFunction tabulatedFunction) {
         this.tabulatedFunction = tabulatedFunction;
@@ -12,7 +12,9 @@ public class ReadTask implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < tabulatedFunction.getCount(); i++) {
-            System.out.printf("After read: i = %dx = %f, y = %f\n", i, tabulatedFunction.getX(i), tabulatedFunction.getY(i));
+            synchronized (tabulatedFunction) {
+                System.out.printf("After read: i = %d, x = %f, y = %f\n", i, tabulatedFunction.getX(i), tabulatedFunction.getY(i));
+            }
         }
     }
 }
--- lab2/src/main/java/ru/ssau/tk/ellapil/lab2/concurrent/WriteTask.java
+++ lab2/src/main/java/ru/ssau/tk/ellapil/lab2/concurrent/WriteTask.java
@@ -3,7 +3,7 @@ package ru.ssau.tk.ellapil.lab2.concurrent;
 import ru.ssau.tk.ellapil.lab2.functions.TabulatedFunction;
 
 public class WriteTask implements Runnable {
-    private TabulatedFunction tabulatedFunction;
+    private final TabulatedFunction tabulatedFunction;
     private double value;
 
     WriteTask(TabulatedFunction tabulatedFunction, double value) {
@@ -14,8 +14,10 @@ public class WriteTask implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < tabulatedFunction.getCount(); i++) {
-            tabulatedFunction.setY(i, value);
-            System.out.printf("Writing for index %d complete\n", i);
+            synchronized (tabulatedFunction) {
+                tabulatedFunction.setY(i, value);
+                System.out.printf("Writing for index %d complete\n", i);
+            }
         }
     }
 }
