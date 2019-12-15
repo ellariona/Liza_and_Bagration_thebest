f9742992422ee6cd0884a5d6a924d445ad9fa6a2
Megrelishvili <bagrat37@mail.ru>
2019-12-15 21:52:44 +0400

    жалкие попытки смертного

---
+++ lab2/src/main/java/ru/ssau/tk/ellapil/lab2/ui/MyFrame.java
@@ -0,0 +1,88 @@
+package ru.ssau.tk.ellapil.lab2.ui;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyFrame extends JFrame {
+    private static final int SPACING_SIZE = 10;
+    private JFrame jFrame = new JFrame("This is frame");
+    List<Double> xValues = new ArrayList<>();
+    List<Double> yValues = new ArrayList<>();
+    AbstractTableModel tableModel = new MyTableModel(xValues, yValues);
+    JTable table = new JTable(tableModel);
+    private JLabel label = new JLabel("Input number of points:");
+    private JTextField countField = new JTextField();
+    private JButton inputButton = new JButton("Input");
+
+    public static void main(String[] args) {
+        MyFrame app = new MyFrame();
+        app.setVisible(true);
+    }
+
+    public MyFrame() {
+        super("Does it work? 0_0");
+        this.setBounds(300, 300, 500, 500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addButtonListeners();
+        compose();
+        inputButton.setEnabled(false);
+    }
+
+    void compose() {
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+        JScrollPane tableScrollPane = new JScrollPane(table);
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+                .addGroup(layout.createSequentialGroup()
+                        .addComponent(label)
+                        .addComponent(countField)
+                        .addComponent(inputButton))
+                .addComponent(tableScrollPane)
+        );
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(label)
+                        .addComponent(countField)
+                        .addComponent(inputButton))
+                .addComponent(tableScrollPane)
+        );
+    }
+
+    public void addButtonListeners() {
+        inputButton.addActionListener(event -> {
+            try {
+                int count = Integer.parseInt(countField.getText());
+                table = new JTable(new MyTableModel(new ArrayList<>(count), new ArrayList<>(count)));
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(this, e.getStackTrace());
+            }
+        });
+
+        countField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                onChanged();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                onChanged();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                onChanged();
+            }
+
+            private void onChanged() {
+                inputButton.setEnabled(!countField.getText().isEmpty());
+            }
+        });
+    }
+}
---
+++ lab2/src/main/java/ru/ssau/tk/ellapil/lab2/ui/MyTableModel.java
@@ -0,0 +1,85 @@
+package ru.ssau.tk.ellapil.lab2.ui;
+
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import java.awt.*;
+import java.util.List;
+
+public class MyTableModel extends AbstractTableModel {
+    private static final int INDEX_COLUMN_NUMBER = 0;
+    private static final int X_COLUMN_NUMBER = 1;
+    private static final int Y_COLUMN_NUMBER = 2;
+    private List<Double> xValues;
+    private List<Double> yValues;
+
+    public MyTableModel(List<Double> xValues, List<Double> yValues) {
+        this.xValues = xValues;
+        this.yValues = yValues;
+    }
+
+    @Override
+    public int getRowCount() {
+        return xValues.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case INDEX_COLUMN_NUMBER:
+                return rowIndex;
+            case X_COLUMN_NUMBER:
+                return xValues.get(rowIndex);
+            case Y_COLUMN_NUMBER:
+                return yValues.get(rowIndex);
+        }
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        if (columnIndex == INDEX_COLUMN_NUMBER) {
+            return;
+        } else if (columnIndex == X_COLUMN_NUMBER) {
+            xValues.set(rowIndex, (Double) aValue);
+        } else {
+            yValues.set(rowIndex, (Double) aValue);
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case INDEX_COLUMN_NUMBER:
+                return false;
+            case X_COLUMN_NUMBER:
+                return true;
+            case Y_COLUMN_NUMBER:
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+            case INDEX_COLUMN_NUMBER:
+                return "Index";
+            case X_COLUMN_NUMBER:
+                return "xValues";
+            case Y_COLUMN_NUMBER:
+                return "yValues";
+        }
+        return super.getColumnName(column);
+    }
+
+//  private Container getContentPane() {
+// }
+}
+
